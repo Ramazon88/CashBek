fd0bd6eb73afc654efe33a1840530591cc91fa96
Ramazon88 <ramazon.sharopov@mail.ru>
2023-06-12 17:38:05 +0500

    main commit5

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,7 +1,7 @@
 # import requests
 # a = requests.post("https://billing.radius.uz:4012/radius_ut/hs/radius_bot/type_report",
 #                   auth=("django_admin", "DJango_96547456"),
-#                   json={"start": "20230519", "end": "20230519"})
+#                   json={"start": "20230611", "end": "20230611"})
 # # b = requests.get("http://80.80.212.224:8080/ut3/hs/radius_bot/cash_flow",
 # #                   headers={"Authorization": "Basic ZGphbmdvX2FkbWluOjE0Nzc4OQ=="},
 # #                   json={"start": "20230428", "end": "20230504"})
